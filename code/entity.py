4cfc4c98798014f43ef9987ec016800fe08d7f9d
marcio520 <141781500+marcio520@users.noreply.github.com>
2025-02-28 11:11:45 -0300

    Adding classes generated from startUML

---
+++ code/entity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.Attribute1 = None
+        self.name = None
+        self.surf = None
+        self.rect = None
+
+    def move(self, ):
+        pass
